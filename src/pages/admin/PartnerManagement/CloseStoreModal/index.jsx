02a1c25f2d87ae5a39a42b16fb0398eaa006a97f
Phan Nguyễn Kim Anh <pkimanh03@gmail.com>
2021-02-05 19:11:29 +0700

    open/close store & complet/cancel order

---
+++ src/pages/admin/PartnerManagement/CloseStoreModal/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from 'dva';
+import { Modal } from 'antd';
+
+class ConfirmationPopup extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { partnerId, storeName, undoneOrder, isOpen } = this.props;
+    return (
+      <Modal
+        title="CONFIRMATION"
+        visible={true}
+        onOk={this.props.hideModal}
+        onCancel={this.props.hideModal}
+        style={{ textAlign: 'center', width: '50%' }}
+        okText="yes"
+        cancelText="no"
+      >
+        {isOpen ? (
+          <p>
+            <b>Open {storeName} store</b>
+          </p>
+        ) : (
+          <div>
+            <p>
+              <b>Close {storeName} store</b>
+            </p>
+            <p>
+              {storeName} has <b>{undoneOrder} incomplete orders</b>
+            </p>
+            <p>If closing, the system will cancel all incomplete orders</p>
+          </div>
+        )}
+      </Modal>
+    );
+  }
+}
+
+export default ConfirmationPopup;
