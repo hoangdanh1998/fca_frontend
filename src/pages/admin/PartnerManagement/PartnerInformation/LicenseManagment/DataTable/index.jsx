ec32ae51c658aa214103f1f196cc28e8d0b5f571
Phan Nguyễn Kim Anh <pkimanh03@gmail.com>
2021-04-01 01:24:37 +0700

    handle pagination

--- src/pages/admin/PartnerManagement/PartnerInformation/LicenseManagment/DataTable/index.jsx
+++ src/pages/admin/PartnerManagement/PartnerInformation/LicenseManagment/DataTable/index.jsx
@@ -24,6 +24,7 @@ class DataTable extends React.Component {
               dataSource={dataList}
               columns={columnList}
               pagination={{
+                showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} records`,
                 current: this.state.page,
                 pageSize: this.state.PAGE_SIZE,
                 total: dataList.length,
